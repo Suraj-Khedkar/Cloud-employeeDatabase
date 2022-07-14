85bd356eff926253738ba0d7d92a6c05ac2ea718
Suraj Khedkar <snk.khedkar@gmail.com>
2022-07-14 12:36:01 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.caza4h1qzz4v.ap-south-1.rds.amazonaws.com"
+customuser = "suraj"
+custompass = "somepassword1"
+customdb = "employee"
+custombucket = "myemployee-data	"
+customregion = "ap-south-1"
